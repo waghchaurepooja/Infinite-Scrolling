4d795534163dff2d241a770f42dd8cb4bb3c4d8a
waghchaurepooja <97495331+waghchaurepooja@users.noreply.github.com>
2022-06-06 20:00:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useEffect, useState } from 'react';
+import Comment from './components/Comments';
+import Loader from './components/Loader';
+import EndMsg from './components/EndMsg';
+
 function App() {
+
+  const [items,setItems]=useState([]);
+  
+  const [noMore,setMore]=useState(true);
+
+const [page,setpage]=useState(2);
+
+
+  useEffect(()=>{
+    const getComments=async () =>{
+
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20`
+      );
+
+      const data =await res.json();
+       setItems (data);
+    };
+     getComments()
+  },[]);
+
+console.log(items);
+
+  const fetchComments= async() =>{
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments?_page=${(page)}&_limit=20`
+    );
+
+    const data =await res.json();
+    return data;
+    };
+
+    const fetchData= async()=>{
+      const commentsFormServer=await fetchComments();
+    
+    setItems([...items,...commentsFormServer]);
+    
+    if(commentsFormServer.length===0 || commentsFormServer.length<20){
+setMore(false);
+}
+setpage(page + 1);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfiniteScroll
+    dataLength={items.length} //This is important field to render the next data
+    next={fetchData}
+    hasMore={noMore}
+    loader={<Loader/>}
+    endMessage={<EndMsg/>
+     
+    }
+  
+  >
+  <div className="container">
+  <div className="row m-2">'
+  { items.map((item)=> {
+     return <Comment key={item.id} item={item}/>;
+  })}
+  </div>
+  </div>
+
+  </InfiniteScroll>
   );
 }
 
---
+++ src/components/EndMsg.js
@@ -0,0 +1,9 @@
+
+
+export default function EndMsg() {
+  return (
+    <p style={{ textAlign: 'center' }}>
+    <b>Yay! You have seen it all</b>
+  </p>
+  )
+}
